68924949448fa2ae2afae9e7d7d20c7a8fa6b33d
Diana Campos <dcampos0495@gmail.com>
2024-05-27 17:25:53 -0600

    delete personal api-key

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -6,7 +6,6 @@ export const movieDBFetcher = new AxiosAdapter ({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
         api_key: THE_MOVIE_DB_KEY ?? 'no-key',
-        //api_key: 'ca82967ddc878052bec5b93623362be7'
 }});
 
 //console.log(movieDBFetcher)
